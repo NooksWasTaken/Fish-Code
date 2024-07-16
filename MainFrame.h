af99594653cb87889c4da01d8c290b7fd8796b62
NooksWasTaken <skoonerkspsn@gmail.com>
2024-07-16 10:27:11 +0800

    Update MainFrame.h

--- MainFrame.h
+++ MainFrame.h
@@ -1,1 +1,20 @@
-Test file
+#include <wx/wx.h>
+#include <wx/gauge.h>
+
+//Creates a class that inherits the wxFrame class from wxWidgets
+
+class MainFrame : public wxFrame {
+public:
+    MainFrame(const wxString& title);
+
+private:
+    void OnButtonClicked(wxCommandEvent& event);
+
+    wxStaticText* counterText;
+    wxStaticText* pointText;
+    wxStaticText* pointTextLabel;
+    wxGauge* healthBar;
+    int counter;
+    int point;
+};
+
